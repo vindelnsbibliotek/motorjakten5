5985be82ecd1cb62c0491240d6082188dcc1ff71
vindelnsbibliotek <96295886+vindelnsbibliotek@users.noreply.github.com>
2022-08-16 16:07:37 +0200

    Digital biblioteksjakt

---
+++ js/contact_me.js
@@ -0,0 +1,13 @@
+$(function() {
+
+  $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
+    preventSubmit: true,
+    submitError: function($form, event, errors) {
+      // additional error messages or events
+    },
+    submitSuccess: function($form, event) {
+      event.preventDefault(); // prevent default submit behaviour
+      // get values from FORM
+      var name = $("input#answer").val();
+
+      });
